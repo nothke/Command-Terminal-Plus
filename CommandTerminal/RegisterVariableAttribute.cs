1eb9a54f641a524fd8f8df3236ffb3e35ddf847e
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-10-25 05:01:34 -0400

    oops forgot this

---
+++ CommandTerminal/RegisterVariableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace CommandTerminalPlus
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RegisterVariableAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public RegisterVariableAttribute(string command_name = null)
+        {
+            Name = command_name;
+        }
+    }
+}
